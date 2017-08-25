ad7f487f22a162ff3f29ca3037227afcf842e00c
Vu Tran <vutran@dropbox.com>
2017-08-24 20:28:09 -0700

    add mat ui

--- src/Hero.js
+++ src/Hero.js
@@ -1,14 +1,21 @@
 import React from 'react';
+import { Card, CardHeader, CardMedia, CardText } from 'material-ui';
 import PropTypes from 'prop-types';
 
 export default class Hero extends React.Component {
 	render() {
 		return (
-			<div className="hero">
-				<img src={this.props.photo} />
-				<h2>{this.props.name}</h2>
-				<div>{this.props.desc}</div>
-			</div>
+			<Card className="hero">
+				<CardHeader>
+					<h2>{this.props.name}</h2>
+				</CardHeader>
+				<CardMedia>
+					<img src={this.props.photo} />
+				</CardMedia>
+				<CardText>
+					<div>{this.props.desc}</div>
+				</CardText>
+			</Card>
 		);
 	}
 }
